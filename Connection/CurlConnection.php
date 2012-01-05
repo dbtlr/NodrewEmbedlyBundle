9fc527d5909fe894bbc966306710963496082ae9
Drew Butler <drew.butler@stylecaster.com>
2012-01-05 18:42:35 -0500

    Abstracted the curl request from the OEmbedClient into the CurlConnection class.

---
+++ Connection/CurlConnection.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Nodrew\Bundle\EmbedlyBundle\Connection;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @package     NodrewEmbedlyBundle
+ * @author      Drew Butler <drew@abstracting.me>
+ * @copyright	(c) 2012 Drew Butler
+ * @license     http://www.opensource.org/licenses/mit-license.php
+ */
+class CurlConnection
+{
+    protected $timeout = 3;
+
+    /**
+     * Build the object, setting the timeout properly.
+     *
+     * @param int $timeout
+     */
+    public function __construct($timeout = 3)
+    {
+        $this->timeout = $timeout;
+    }
+    
+	/**
+	 * @param string $path
+	 * @return string
+	 **/
+	public function request($path)
+	{
+		$curl = curl_init();
+
+		curl_setopt($curl, CURLOPT_URL, $path);
+		curl_setopt($curl, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $return = curl_exec($curl);
+        $code   = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+ 
+		curl_close($curl);
+        
+        if ($code != 200) {
+            $return = $this->buildErrorReturn($code);
+        }
+
+		return $return;
+	}
+	
+	/**
+	 * Given the current response code, create and error response json type.
+	 *
+	 * @param int $code
+	 * @return stringr
+	 */
+	protected function buildErrorReturn($code)
+	{
+	    return json_encode(array(
+	        'type'          => 'error',
+	        'error_code'    => $code,
+	        'error_message' => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : 'Unknown response code',
+	    ));
+    }
+}
--- Service/OEmbedClient.php
+++ Service/OEmbedClient.php
@@ -4,6 +4,7 @@ namespace Nodrew\Bundle\EmbedlyBundle\Service;
 
 use Nodrew\Bundle\EmbedlyBundle\Model\QueryArguments,
     Nodrew\Bundle\EmbedlyBundle\Factory\ResponseFactory,
+    Nodrew\Bundle\EmbedlyBundle\Connection\CurlConnection,
     Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -16,7 +17,7 @@ class OEmbedClient
 {
     const CLIENT_URI = 'http://api.embed.ly/1/oembed?%s';
     protected $queryArguments;
-    protected $timeout;
+    protected $connection;
 
     /**
      * @param string $key
@@ -24,22 +25,32 @@ class OEmbedClient
      */
     public function __construct($key, $timeout = 3, $defaultOptions = array())
     {
-        $this->timeout = $timeout;
+        $this->connection = new CurlConnection($timeout);
 
         $this->queryArguments = new QueryArguments((array) $defaultOptions);
         $this->queryArguments->setKey($key);
     }
 
     /**
+     * Set the cURL connection object to use.
+     *
+     * @param mixed $conn
+     */
+    public function setCurlConnection($conn)
+    {
+        $this->connection = $conn;
+    }
+
+    /**
      * Fetch from embedly, based on the given url(s). If multiple are given in an array, then an array of responses will be returned.
-     * 
+     *
      * @param string|array $url
      * @return array|null
      */
     public function fetch($url)
     {
         $queryArgs = clone $this->queryArguments;
-        
+
         if (is_array($url)) {
             $queryArgs->setUrls($url);
 
@@ -47,50 +58,11 @@ class OEmbedClient
             $queryArgs->setUrl($url);
         }
 
-        if (!$response = $this->request($queryArgs)) {
+        if (!$response = $this->connection->request(sprintf(self::CLIENT_URI, $queryArgs))) {
             return;
         }
-        
+
         return $this->parseResponse($response);
-    }
-
-	/**
-	 * @param Nodrew\Bundle\EmbedlyBundle\Model\QueryArguments $queryParams
-	 * @return string
-	 **/
-	protected function request(QueryArguments $queryArgs)
-	{
-		$curl = curl_init();
-
-		curl_setopt($curl, CURLOPT_URL, sprintf(self::CLIENT_URI, $query));
-		curl_setopt($curl, CURLOPT_TIMEOUT, $this->timeout);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-        $return = curl_exec($curl);
-        $code   = curl_getinfo($curl, CURLINFO_HTTP_CODE);
- 
-		curl_close($curl);
-        
-        if ($code != 200) {
-            $return = $this->buildErrorReturn($code);
-        }
-
-		return $return;
-	}
-	
-	/**
-	 * Given the current response code, create and error response json type.
-	 *
-	 * @param int $code
-	 * @return stringr
-	 */
-	protected function buildErrorReturn($code)
-	{
-	    return json_encode(array(
-	        'type'          => 'error',
-	        'error_code'    => $code,
-	        'error_message' => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : 'Unknown response code',
-	    ));
     }
 
 	/**
@@ -105,7 +77,7 @@ class OEmbedClient
 	        return;
         }
 
-	    $factory  = new ResponseFactory;
+	    $factory = new ResponseFactory;
 
 	    if (!$this->hasMultipleResponses($decoded)) {
 	        return $factory->buildResponse($decoded);
@@ -118,7 +90,7 @@ class OEmbedClient
 
         return $responses;
     }
-    
+
     /**
      * Check to see if the given response has multiple records in it.
      *
@@ -128,5 +100,5 @@ class OEmbedClient
     protected function hasMultipleResponses($response)
     {
         return array_keys($response) === range(0, count($response) - 1);
-    }    
+    }
 }
