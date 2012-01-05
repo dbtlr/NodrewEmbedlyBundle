b4c47ac11a5b955851ea4365229787b1d06b2340
Drew Butler <drew.butler@stylecaster.com>
2012-01-05 16:29:06 -0500

    Added basic unit test loader.

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nodrew\Bundle\EmbedlyBundle\Tests\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Nodrew\Bundle\EmbedlyBundle\DependencyInjection\Configuration;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testThatCanGetConfigTree()
+    {
+        $configuration = new Configuration();
+        $this->assertInstanceOf('Symfony\Component\Config\Definition\NodeInterface', $configuration->getConfigTree());
+    }
+}
