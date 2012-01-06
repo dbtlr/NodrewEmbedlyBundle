8d1f0842c174301e580d271e1f9faa5e8715603f
Drew Butler <drew@abstracting.me>
2012-01-06 12:06:22 -0500

    Moved the logic from the OEmbedClient class into a Client abstract.

---
+++ Service/Client.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Nodrew\Bundle\EmbedlyBundle\Service;
+
+use Nodrew\Bundle\EmbedlyBundle\Model\QueryArguments,
+    Nodrew\Bundle\EmbedlyBundle\Factory\ResponseFactory,
+    Nodrew\Bundle\EmbedlyBundle\Connection\CurlConnection,
+    Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @package     NodrewEmbedlyBundle
+ * @author      Drew Butler <drew@abstracting.me>
+ * @copyright	(c) 2012 Drew Butler
+ * @license     http://www.opensource.org/licenses/mit-license.php
+ */
+abstract class Client
+{
+    protected $clientUri;
+    protected $queryArguments;
+    protected $connection;
+
+    /**
+     * @param string $key
+     * @param array $defaultOptions
+     */
+    public function __construct($key, $timeout = 3, $defaultOptions = array())
+    {
+        if (!defined('static::CLIENT_URI')) {
+            throw new \LogicException('The client class '.get_class($this).' should have the constant CLIENT_URI defined, but does not.');
+        }
+
+        $this->clientUri  = static::CLIENT_URI;
+        $this->connection = new CurlConnection($timeout);
+
+        $this->queryArguments = new QueryArguments((array) $defaultOptions);
+        $this->queryArguments->setKey($key);
+    }
+
+    /**
+     * Set the cURL connection object to use.
+     *
+     * @param mixed $conn
+     */
+    public function setCurlConnection($conn)
+    {
+        $this->connection = $conn;
+    }
+
+    /**
+     * Fetch from embedly, based on the given url(s). If multiple are given in an array, then an array of responses will be returned.
+     *
+     * @param string|array $url
+     * @return array|null
+     */
+    public function fetch($url)
+    {
+        $queryArgs = clone $this->queryArguments;
+
+        if (is_array($url)) {
+            $queryArgs->setUrls($url);
+
+        } else {
+            $queryArgs->setUrl($url);
+        }
+
+        if (!$response = $this->connection->request(sprintf($this->clientUri, $queryArgs))) {
+            return;
+        }
+
+        return $this->parseResponse($response);
+    }
+
+	/**
+	 * Parse the given response JSON.
+	 *
+	 * @param string $textResponse
+	 * @return array|null
+	 */
+	protected function parseResponse($textResponse)
+	{
+	    if (!$decoded = json_decode($textResponse, true)) {
+	        return;
+        }
+
+	    $factory = new ResponseFactory;
+
+	    if (!$this->hasMultipleResponses($decoded)) {
+	        return $factory->buildResponse($decoded);
+        }
+
+        $responses = array();
+        foreach ($decoded as $response) {
+            $responses[] = $factory->buildResponse($response);
+        }
+
+        return $responses;
+    }
+
+    /**
+     * Check to see if the given response has multiple records in it.
+     *
+     * @param array $response
+     * @return bool
+     */
+    protected function hasMultipleResponses($response)
+    {
+        return array_keys($response) === range(0, count($response) - 1);
+    }
+}
--- Tests/Service/ClientTest.php
+++ Tests/Service/ClientTest.php
@@ -2,17 +2,26 @@
 
 namespace Nodrew\Bundle\EmbedlyBundle\Tests\Service;
 
-use Nodrew\Bundle\EmbedlyBundle\Service\OEmbedClient,
+use Nodrew\Bundle\EmbedlyBundle\Service\Client,
     Nodrew\Bundle\EmbedlyBundle\Connection\CurlConnection;
 
-class OEmbedClientTest extends \PHPUnit_Framework_TestCase
+class ClientTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @covers Nodrew\Bundle\EmbedlyBundle\Service\OEmbedClient::fetch
+     * @covers Nodrew\Bundle\EmbedlyBundle\Service\Client::__construct
+     */
+    public function testConstructWillExplodeIfChildClientDoesNotHaveAUriDefined()
+    {
+        $this->setExpectedException('LogicException');
+        $client = new BadMockClient('keynum', 4, array('wmode' => 'window'));
+    }    
+    
+    /**
+     * @covers Nodrew\Bundle\EmbedlyBundle\Service\Client::fetch
      */
     public function testWillBuildObjectsThroughTheFactoryBasedOnTypeGiven()
     {
-        $client = new OEmbedClient('keynum', 4, array('wmode' => 'window'));
+        $client = new MockClient('keynum', 4, array('wmode' => 'window'));
         $client->setCurlConnection($this->getMockConn(array('type' => 'link')));
 
         $result = $client->fetch('http://www.example.com');
@@ -21,11 +30,11 @@ class OEmbedClientTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Nodrew\Bundle\EmbedlyBundle\Service\OEmbedClient::fetch
+     * @covers Nodrew\Bundle\EmbedlyBundle\Service\Client::fetch
      */
     public function testWillBuildMultipleObjectsThroughTheFactoryWhenReturnedMultipleResults()
     {
-        $client = new OEmbedClient('keynum', 4, array('wmode' => 'window'));
+        $client = new MockClient('keynum', 4, array('wmode' => 'window'));
         $client->setCurlConnection($this->getMockConn(array(array('type' => 'link'), array('type' => 'video'))));
 
         $result = $client->fetch('http://www.example.com');
@@ -38,11 +47,11 @@ class OEmbedClientTest extends \PHPUnit_Framework_TestCase
     
 
     /**
-     * @covers Nodrew\Bundle\EmbedlyBundle\Service\OEmbedClient::fetch
+     * @covers Nodrew\Bundle\EmbedlyBundle\Service\Client::fetch
      */
     public function testWillReturnErrorObjectIfErrorCodeReturned()
     {
-        $client = new OEmbedClient('keynum', 4, array('wmode' => 'window'));
+        $client = new MockClient('keynum', 4, array('wmode' => 'window'));
         $client->setCurlConnection($this->getMockConn(404));
 
         $result = $client->fetch('http://www.example.com');
@@ -53,31 +62,31 @@ class OEmbedClientTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Nodrew\Bundle\EmbedlyBundle\Service\OEmbedClient::fetch
+     * @covers Nodrew\Bundle\EmbedlyBundle\Service\Client::fetch
      */
     public function testWillSetSingleUrlProperly()
     {
-        $client = new OEmbedClient('keynum', 4, array('wmode' => 'window'));
+        $client = new MockClient('keynum', 4, array('wmode' => 'window'));
         $conn   = $this->getMockConn(array('type' => 'link'));
         $client->setCurlConnection($conn);
 
         $result = $client->fetch('http://www.example.com');
 
-        $this->assertEquals('http://api.embed.ly/1/oembed?key=keynum&url=http%3A%2F%2Fwww.example.com&format=json&wmode=window&nostyle=0&autoplay=0&videosrc=0&words=50', $conn->path);
+        $this->assertEquals('http://www.example.com/blah?key=keynum&url=http%3A%2F%2Fwww.example.com&format=json&wmode=window&nostyle=0&autoplay=0&videosrc=0&words=50', $conn->path);
     }
 
     /**
-     * @covers Nodrew\Bundle\EmbedlyBundle\Service\OEmbedClient::fetch
+     * @covers Nodrew\Bundle\EmbedlyBundle\Service\Client::fetch
      */
     public function testWillSetMultipleUrlsProperly()
     {
-        $client = new OEmbedClient('keynum', 4, array('wmode' => 'window'));
+        $client = new MockClient('keynum', 4, array('wmode' => 'window'));
         $conn   = $this->getMockConn(array('type' => 'link'));
         $client->setCurlConnection($conn);
 
         $result = $client->fetch(array('http://www.example.com', 'http://www.example1.com'));
 
-        $this->assertEquals('http://api.embed.ly/1/oembed?key=keynum&urls=http%3A%2F%2Fwww.example.com,http%3A%2F%2Fwww.example1.com&format=json&wmode=window&nostyle=0&autoplay=0&videosrc=0&words=50', $conn->path);
+        $this->assertEquals('http://www.example.com/blah?key=keynum&urls=http%3A%2F%2Fwww.example.com,http%3A%2F%2Fwww.example1.com&format=json&wmode=window&nostyle=0&autoplay=0&videosrc=0&words=50', $conn->path);
     }
 
     /**
@@ -96,6 +105,22 @@ class OEmbedClientTest extends \PHPUnit_Framework_TestCase
 
         return $conn;
     }
+}
+
+/**
+ * A mock client WITH the CLIENT_URL defined as required.
+ */
+class MockClient extends Client
+{
+    const CLIENT_URI = 'http://www.example.com/blah?%s';
+}
+
+/**
+ * A mock client WITHOUT the CLIENT_URL defined as required.
+ */
+class BadMockClient extends Client
+{
+
 }
 
 /**
